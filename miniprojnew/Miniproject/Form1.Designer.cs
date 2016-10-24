306bd22cec890f51e80170b5e587faeed1e2ab34
GowthamGoud <p.gowthamgoud@gmail.com>
2016-10-24 18:44:12 +0530

    Add files via upload

---
+++ miniprojnew/Miniproject/Form1.Designer.cs
@@ -0,0 +1,246 @@
+﻿namespace Miniproject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textLocalport = new System.Windows.Forms.TextBox();
+            this.textLocalip = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textFreindport = new System.Windows.Forms.TextBox();
+            this.textFriendIp = new System.Windows.Forms.TextBox();
+            this.listmessage = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textmsg = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.textLocalport);
+            this.groupBox1.Controls.Add(this.textLocalip);
+            this.groupBox1.Location = new System.Drawing.Point(24, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(261, 122);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "sender";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(19, 86);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 16);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "port";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(19, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(19, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "ip";
+            // 
+            // textLocalport
+            // 
+            this.textLocalport.Location = new System.Drawing.Point(113, 86);
+            this.textLocalport.Name = "textLocalport";
+            this.textLocalport.Size = new System.Drawing.Size(100, 20);
+            this.textLocalport.TabIndex = 1;
+            // 
+            // textLocalip
+            // 
+            this.textLocalip.Location = new System.Drawing.Point(113, 33);
+            this.textLocalip.Name = "textLocalip";
+            this.textLocalip.Size = new System.Drawing.Size(100, 20);
+            this.textLocalip.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.textFreindport);
+            this.groupBox2.Controls.Add(this.textFriendIp);
+            this.groupBox2.Location = new System.Drawing.Point(301, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(274, 122);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "receiver";
+            this.groupBox2.Enter += new System.EventHandler(this.groupBox2_Enter);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(6, 79);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(31, 16);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "port";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(6, 26);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(19, 16);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "ip";
+            // 
+            // textFreindport
+            // 
+            this.textFreindport.Location = new System.Drawing.Point(99, 74);
+            this.textFreindport.Name = "textFreindport";
+            this.textFreindport.Size = new System.Drawing.Size(100, 20);
+            this.textFreindport.TabIndex = 2;
+            // 
+            // textFriendIp
+            // 
+            this.textFriendIp.Location = new System.Drawing.Point(99, 19);
+            this.textFriendIp.Name = "textFriendIp";
+            this.textFriendIp.Size = new System.Drawing.Size(100, 20);
+            this.textFriendIp.TabIndex = 1;
+            // 
+            // listmessage
+            // 
+            this.listmessage.FormattingEnabled = true;
+            this.listmessage.Location = new System.Drawing.Point(24, 149);
+            this.listmessage.Name = "listmessage";
+            this.listmessage.Size = new System.Drawing.Size(604, 147);
+            this.listmessage.TabIndex = 2;
+            this.listmessage.SelectedIndexChanged += new System.EventHandler(this.listmessage_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(581, 83);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textmsg
+            // 
+            this.textmsg.Location = new System.Drawing.Point(24, 336);
+            this.textmsg.Name = "textmsg";
+            this.textmsg.Size = new System.Drawing.Size(382, 20);
+            this.textmsg.TabIndex = 4;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(412, 319);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 52);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "send";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(662, 31);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(266, 277);
+            this.listBox1.TabIndex = 7;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(500, 317);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 54);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Send files";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(961, 397);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textmsg);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listmessage);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textLocalport;
+        private System.Windows.Forms.TextBox textLocalip;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textFreindport;
+        private System.Windows.Forms.TextBox textFriendIp;
+        private System.Windows.Forms.ListBox listmessage;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textmsg;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ miniprojnew/Miniproject/Form1.cs
@@ -0,0 +1,469 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Diagnostics;
+using System.Web.Services.Protocols;
+using System.IO;
+namespace Miniproject
+{
+    public partial class Form1 : Form
+    {
+
+        Socket sck;
+        EndPoint eplocal, epremote;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+
+
+
+
+
+
+
+
+
+
+          
+
+
+
+            IPGlobalProperties computerProperties = IPGlobalProperties.GetIPGlobalProperties();
+            NetworkInterface[] nics = NetworkInterface.GetAllNetworkInterfaces();
+
+
+
+            listBox1.Items.Add("Interface information for " + computerProperties.HostName + computerProperties.DomainName);
+            if (nics == null || nics.Length < 1)
+            {
+                listBox1.Items.Add("  No network interfaces found.");
+                return;
+            }
+
+            listBox1.Items.Add(nics.Length);
+            foreach (NetworkInterface adapter in nics)
+            {
+                IPInterfaceProperties properties = adapter.GetIPProperties(); //  .GetIPInterfaceProperties();
+                listBox1.Items.Add("");
+                listBox1.Items.Add(adapter.Description);
+                listBox1.Items.Add(String.Empty.PadLeft(adapter.Description.Length, '='));
+                listBox1.Items.Add(adapter.NetworkInterfaceType);
+                listBox1.Items.Add("  Physical address ........................ : ");
+                PhysicalAddress address = adapter.GetPhysicalAddress();
+                byte[] bytes = address.GetAddressBytes();
+                string str1 = "";
+                for (int i = 0; i < bytes.Length; i++)
+                {
+
+                    str1 = str1 + bytes[i].ToString("X2");
+                    // Display the physical address in hexadecimal.
+                    //listBox1.Items.Add(bytes[i].ToString("X2"));
+                    // Insert a hyphen after each byte, unless we are at the end of the  
+                    // address. 
+                    if (i != bytes.Length - 1)
+                    {
+                        str1 = str1 + "-";
+                        //listBox1.Items.Add("-");
+                    }
+                }
+                listBox1.Items.Add(str1);
+            }
+
+
+
+
+
+
+
+
+
+
+
+            sck = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            //
+            sck.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
+            //to get the ip addresses into the textboxes
+            //local ip address
+            textLocalip.Text = getLocalIP();
+            //receiver ip address
+            //textFriendIp.Text = getLocalIP();
+            // IPList2();
+
+            //computername of this system 
+            string computerName = System.Net.Dns.GetHostName();
+            listBox1.Items.Add(computerName);
+
+            
+
+
+
+
+
+            System.Net.IPHostEntry ipEntry = System.Net.Dns.GetHostEntry(computerName);
+            System.Net.IPAddress[] ipAddress = ipEntry.AddressList;
+            try
+            {
+                listBox1.Items.Add(ipAddress[2]);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("No lan connection! \n" + e);
+            }
+
+
+            for (int i = 0; i < ipAddress.Length; i++)
+            {
+                listBox1.Items.Add(ipAddress[i]);
+            }
+            computerName = computerName + "\t" + ipAddress[0].ToString();
+            listBox1.Items.Add(computerName);
+
+
+
+
+
+            //Read file to byte array
+
+          
+
+       }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        private static PhysicalAddress GetMacAddress()
+        {
+            foreach (NetworkInterface nic in NetworkInterface.GetAllNetworkInterfaces())
+            {
+                // Only consider Ethernet network interfaces
+                if (nic.NetworkInterfaceType == NetworkInterfaceType.Ethernet &&
+                    nic.OperationalStatus == OperationalStatus.Up)
+                {
+                    return nic.GetPhysicalAddress();
+                }
+            }
+            return null;
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        private static IPAddress GetIPAddress()
+        {
+            String strHostName = Dns.GetHostName();
+            IPHostEntry ipEntry = Dns.GetHostEntry(strHostName);
+            IPAddress[] addr = ipEntry.AddressList;
+            foreach (IPAddress ip in addr)
+            {
+                if (!ip.IsIPv6LinkLocal)
+                {
+                    return (ip);
+                }
+            }
+            return addr.Length > 0 ? addr[0] : null;
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        private static bool IsMulticast(IPAddress ip)
+        {
+            bool result = true;
+            if (!ip.IsIPv6Multicast)
+            {
+                byte highIP = ip.GetAddressBytes()[0];
+                if (highIP < 224 || highIP > 239)
+                {
+                    result = false;
+                }
+            }
+            return result;
+        }
+
+        //for ipaddresses of lan connected systems
+        /*
+        private delegate void MyPing(int id);
+                public string IPList2()
+                {
+
+                    string myipsplit = string.Empty;
+                    string localhostname = Dns.GetHostName();
+                    IPAddress[] paddresses = Dns.GetHostAddresses(localhostname);
+                    string myip = paddresses.Where(ip => ip.AddressFamily == AddressFamily.InterNetwork).FirstOrDefault().ToString();
+                    string[] myiparray = myip.Split(new[] { '.' });
+                    for (int j = 1; j < myiparray.Length; j++)
+                        myipsplit += myiparray[j - 1] + ".";
+                    Trace.WriteLine(DateTime.Now);
+                    var results = new string[0x100];
+                    MyPing ping = 
+                     id =>
+                    {
+                        string ls = myipsplit + id;
+                        var pingSender = new Ping();
+                        PingReply reply = pingSender.Send(ls, 100);
+                        if (reply != null)
+                            if (reply.Status == IPStatus.Success)
+                                results[id] = reply.Address.ToString();
+                    };
+                    var asyncResults = new IAsyncResult[0x100];
+                    for (int i = 1; i < 0x100; i++)
+                    {
+                        asyncResults[i] = ping.BeginInvoke(i, null, null);
+                    }
+                    for (int i = 1; i < 0x100; i++)
+                    {
+                        ping.EndInvoke(asyncResults[i]);
+                    }
+                    Trace.WriteLine(DateTime.Now);
+                    var sb = new StringBuilder();
+                    for (int i = 1; i < 0x100; i++)
+                    {
+                        if (results[i] != null)
+                            listBox1.Items.Add(results[i]);
+                            sb.AppendFormat("{0} ", results[i]);
+                    }
+                    return sb.ToString();
+                }
+              */
+
+        private string getLocalIP()
+        {
+            // method for getting ip address
+            IPHostEntry host;
+            host = Dns.GetHostEntry(Dns.GetHostName());
+
+            foreach (IPAddress ip in host.AddressList)
+            {
+
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    //network ip address is returned in the form of string
+                    return ip.ToString();
+                }
+            }
+            //if nothing is there to be returned then this is returned
+            return "127.0.0.1";
+        }
+
+
+
+
+
+        private void messageCallBack(IAsyncResult aResult)
+        {
+            //method for callback
+            try
+            {
+                int size = sck.EndReceiveFrom(aResult, ref epremote);
+
+                //if there is inf or not
+                if (size > 0)
+                {
+                    byte[] receivedData = new byte[1464];
+
+                    receivedData = (byte[])aResult.AsyncState;
+                    ASCIIEncoding eEncoding = new ASCIIEncoding();
+                    //converts the received bytes into string
+                    using (Stream file = File.OpenWrite(@"e:\filescopy"))
+                    {
+                        file.Write(receivedData, 0, receivedData.Length);
+                    }
+                    //string receivedMessage = eEncoding.GetString(receivedData);
+                    //displaying the message in the listbox
+                   // listmessage.Items.Add("friend:" + receivedMessage);
+                }
+                byte[] buffer = new byte[1500];
+                sck.BeginReceiveFrom(buffer, 0, buffer.Length, SocketFlags.None, ref epremote, new AsyncCallback(messageCallBack), buffer);
+
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.ToString());
+            }
+        }
+
+        private void groupBox2_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //convert into bytes
+                System.Text.ASCIIEncoding enc = new System.Text.ASCIIEncoding();
+                byte[] msg = new byte[1500];
+                msg = enc.GetBytes(textmsg.Text);
+                
+                //send the msg to other client in the form of bytes
+                //Read file to byte array
+
+                FileStream stream = File.OpenRead(textmsg.Text);
+                byte[] fileBytes = new byte[stream.Length];
+                stream.Read(fileBytes, 0, fileBytes.Length);
+
+                sck.Send(fileBytes);
+
+               // sck.Send(msg);
+                // we need to see msg in list box
+                listmessage.Items.Add("you:" + textmsg.Text);
+                //clear the text 
+                textmsg.Clear();
+
+            }
+            catch (Exception e2)
+            {
+                MessageBox.Show(e2.ToString());
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                eplocal = new IPEndPoint(IPAddress.Parse(textLocalip.Text), Convert.ToInt32(textLocalport.Text));
+                sck.Bind(eplocal);
+
+                //receiving
+                epremote = new IPEndPoint(IPAddress.Parse(textFriendIp.Text), Convert.ToInt32(textFreindport.Text));
+                sck.Connect(epremote);
+                byte[] buffer = new byte[1500];
+                sck.BeginReceiveFrom(buffer, 0, buffer.Length, SocketFlags.None, ref epremote, new AsyncCallback(messageCallBack), buffer);
+                // click start button ip is connected message should be shown
+                button1.Text = "connected";
+                button1.Enabled = false;
+                button2.Enabled = true;
+                textmsg.Focus();
+            }
+            catch (Exception e1)
+            {
+                MessageBox.Show(e1.ToString());
+            }
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            /*// Create a new instance of FolderBrowserDialog.
+            FolderBrowserDialog folderBrowserDlg = new FolderBrowserDialog();
+            // A new folder button will display in FolderBrowserDialog.
+            folderBrowserDlg.ShowNewFolderButton = true;
+            //Show FolderBrowserDialog
+            DialogResult dlgResult = folderBrowserDlg.ShowDialog();
+            if (dlgResult.Equals(DialogResult.OK))
+            {
+                //Show selected folder path in textbox1.
+               // textBox1.Text = folderBrowserDlg.SelectedPath;
+                //Browsing start from root folder.
+                Environment.SpecialFolder rootFolder = folderBrowserDlg.RootFolder;*/
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            openFileDialog1.InitialDirectory = @"E:\";
+            openFileDialog1.Title = "Browse Text Files";
+
+            openFileDialog1.CheckFileExists = true;
+            openFileDialog1.CheckPathExists = true;
+
+            openFileDialog1.DefaultExt = "txt";
+            openFileDialog1.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+            openFileDialog1.FilterIndex = 2;
+            openFileDialog1.RestoreDirectory = true;
+
+            openFileDialog1.ReadOnlyChecked = true;
+            openFileDialog1.ShowReadOnly = true;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textmsg.Text = openFileDialog1.FileName;
+            }
+        }
+
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listmessage_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+    }
+}
+
